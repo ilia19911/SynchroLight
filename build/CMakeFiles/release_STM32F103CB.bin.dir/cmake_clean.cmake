55c56051ecb124c1652c3fac9e89fedadcd13232
ostanin_ie <ilia1991@icloud.com>
2021-05-26 23:41:53 +0300

    добавил гитигноре

--- build/CMakeFiles/release_STM32F103CB.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/release_STM32F103CB.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/release_STM32F103CB.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/release_STM32F103CB.hex.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/release_STM32F103CB.hex"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/release_STM32F103CB.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
