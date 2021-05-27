00dedf516940a95cf40b01c3d87affc1cfd870c4
ostanin_ie <ilia1991@icloud.com>
2021-05-27 15:03:55 +0300

    удалил сам скрипт добавления sha из индекса

--- script_add_git_hash.sh
+++
@@ -1,5 +0,0 @@
-DIR=${1}
-SHA=$(git rev-parse HEAD)
-
-printf "#ifndef _GIT_SHA_STRING_H_ \n #define _GIT_SHA_STRING_H_ \n char git_sha_string[] = \"%s\" ; \n #endif \n" $DIR$SHA > $DIR/git_sha_string.h
-
